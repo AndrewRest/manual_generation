62ba3d15b013189e85513c4480c6961357558ca8
nikitasenko <nikita.senko@mifort.org>
2018-11-08 13:40:54 +0300

    add argv and winMac.pdf

--- README.md
+++ README.md
@@ -2,7 +2,14 @@
 1) npm install
 
 ## Run script
-2) node pdf_generate.js
+2) node pdf_generate.js --win=pdf/win.pdf --mac=pdf/mac.pdf --winMac=pdf/winMac.pdf
 
-Source file at the moment - index2.html
+Script get next argv:
+1. win - path and name win pdf
+2. mac - path and name mac pdf
+3. winMac - path and name win and mac pdf
+
+Default path and name - pdf/win.pdf || pdf/osx.pdf || pdf/winMac.pdf
+
+Source file at the moment - HowToInstall.html
 Result files in 'pdf' folder
--- pdf_generate.js
+++ pdf_generate.js
@@ -1,29 +1,37 @@
-let fs = require('fs');
-let path = require('path')
-let pdf = require('html-pdf');
-let html = fs.readFileSync('./src/index2.html', 'utf8');
-let options = require('./config');
-
+const pdf = require('html-pdf');
+const fs = require('fs');
+const html = fs.readFileSync('./src/HowToInstall.html', 'utf8');
+const options = require('./config');
 const ADDITIONAL_CSS_PLACEHOLDER = '{{additional_css}}';
 const STATIC_FILE_PATH = 'file:///' + __dirname.replace(/\\/g,'/') + '/src/';
+const argv = require('minimist')(process.argv.slice(2));
 
 console.log('Create html for Windows...');
-let winHtml = html.replace(ADDITIONAL_CSS_PLACEHOLDER, STATIC_FILE_PATH + 'win.css');
+const winHtml = html.replace(ADDITIONAL_CSS_PLACEHOLDER, STATIC_FILE_PATH + 'win.css');
 console.log('Created');
 
 console.log('Create html for OSX...');
-let osxHtml = html.replace(ADDITIONAL_CSS_PLACEHOLDER, STATIC_FILE_PATH + 'osx.css');
+const osxHtml = html.replace(ADDITIONAL_CSS_PLACEHOLDER, STATIC_FILE_PATH + 'osx.css');
 console.log('Created');
 
-pdf.create(winHtml, options).toFile('pdf/win.pdf', function(err, res) {
+console.log('Create html for Win and Mac');
+const winOsxHtml = html.replace(ADDITIONAL_CSS_PLACEHOLDER, STATIC_FILE_PATH);
+
+pdf.create(winHtml, options).toFile(argv.win || 'pdf/win.pdf', (err, res) => {
   if (err) return console.error("Error during win.pdf creation", err);
   console.log("win.pdf is created");
   console.log(res);
+});
 
-  pdf.create(osxHtml, options).toFile('pdf/osx.pdf', function(err, res) {
-      if (err) return console.error("Error during osx.pdf creation", err);
-      console.log("osx.pdf is created");
-      console.log(res);
-    });
+pdf.create(osxHtml, options).toFile(argv.mac || 'pdf/osx.pdf', (err, res) => {
+  if (err) return console.error("Error during osx.pdf creation", err);
+  console.log("osx.pdf is created");
+  console.log(res);
+});
+
+pdf.create(winOsxHtml, options).toFile(argv.winMac || 'pdf/winMac.pdf', (err, res) => {
+  if (err) return console.error("Error during winAndOSx.pdf creation", err);
+  console.log("winAndOSX.pdf is created");
+  console.log(res);
 });
 
