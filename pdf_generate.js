ddf1c86c0d870c480b51e7f1b4de0ec6e15988db
nikitasenko <nikita.senko@mifort.org>
2018-11-09 17:02:14 +0300

    refactoring

--- pdf_generate.js
+++ pdf_generate.js
@@ -14,47 +14,38 @@ const qAt = argv.source.indexOf("?");
 
 if (qAt === -1) {
     const html = fs.readFileSync('./src/'+ argv.source, 'utf8');
-    createForWinAndOSX(html);
+    createPDF(html);
 } else {
-    const param = argv.source.substr(qAt+1).toLowerCase();
+    const osType = argv.source.substr(qAt+1).toLowerCase();
     const htmlName=argv.source.substr(0, qAt);
     const html = fs.readFileSync('./src/'+ htmlName, 'utf8');
-    if (param === WIN) createForWin(html);
-    if (param === OSX) createForOSX(html);
+    createPDF(html, osType);
 }
 
-function createForWin(html) {
-    console.log('Create html for Windows...');
-    const winHtml = html.replace(ADDITIONAL_CSS_PLACEHOLDER, STATIC_FILE_PATH + 'win.css');
-    console.log('Created');
-
-    pdf.create(winHtml, options).toFile(argv.target || 'pdf/win.pdf', (err, res) => {
-        if (err) return console.error("Error during win.pdf creation", err);
-        console.log("win.pdf is created");
-        console.log(res);
-    });
+function createPDF(html, osType) {
+    console.log('Create html');
+    if (osType) {
+        if (osType === WIN) {
+            const osTypeHtml = html.replace(ADDITIONAL_CSS_PLACEHOLDER, STATIC_FILE_PATH + 'win.css');
+            console.log('Created');
+            generatePDF(osTypeHtml, argv.target || 'pdf/win.pdf')
+        }
+        if (osType === OSX) {
+            const osTypeHtml = html.replace(ADDITIONAL_CSS_PLACEHOLDER, STATIC_FILE_PATH + 'osx.css');
+            console.log('Created');
+            generatePDF(osTypeHtml, argv.target || 'pdf/osx.pdf')
+        }
+    } else {
+        const osTypeHtml = html.replace(ADDITIONAL_CSS_PLACEHOLDER, STATIC_FILE_PATH);
+        console.log('Created');
+        generatePDF(osTypeHtml, argv.target || 'pdf/winOSX.pdf')
+    }
 }
 
-function createForOSX(html) {
-    console.log('Create html for OSX...');
-    const osxHtml = html.replace(ADDITIONAL_CSS_PLACEHOLDER, STATIC_FILE_PATH + 'osx.css');
-    console.log('Created');
-
-    pdf.create(osxHtml, options).toFile(argv.target || 'pdf/osx.pdf', (err, res) => {
-        if (err) return console.error("Error during osx.pdf creation", err);
-        console.log("osx.pdf is created");
-        console.log(res);
-    });
-}
-
-function createForWinAndOSX(html) {
-    console.log('Create html for Win and OSX');
-    const winOsxHtml = html.replace(ADDITIONAL_CSS_PLACEHOLDER, STATIC_FILE_PATH);
-    console.log('Created');
-
-    pdf.create(winOsxHtml, options).toFile(argv.target || 'pdf/winOSX.pdf', (err, res) => {
+function generatePDF(osTypeHtml, path) {
+    pdf.create(osTypeHtml, options).toFile(path, (err, res) => {
         if (err) return console.error("Error during winAndOSx.pdf creation", err);
-        console.log("winAndOSX.pdf is created");
+        console.log("Pdf is created");
         console.log(res);
     });
 }
